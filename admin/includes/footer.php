635fd1b5402fe9ff71313550eb97883e979ea45c
Alam-Monir <monirrza0641@gmail.com>
2024-10-23 04:21:37 +0530

    changed realtive path to absolute paths for fix

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,6 +1,6 @@
 <!-- this is reuseable footer -->
 
-  <script src="assets/js/jquery-3.7.1.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="/break-point/admin/assets/js/jquery-3.7.1.min.js"></script>
+    <script src="/break-point/admin/assets/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -6,6 +6,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Page</title>
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/break-point/admin/assets/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body>
