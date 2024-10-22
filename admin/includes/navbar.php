5091577ad46e5951a979c6f750e579d6bb14b784
Alam-Monir <monirrza0641@gmail.com>
2024-10-23 04:23:20 +0530

    added logout feature and links to pages

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -1,9 +1,24 @@
 <!-- This is a reuseable navbar -->
+<?php
+
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+
+    session_unset();
+    session_destroy();
+    
+    echo "<script>
+        alert('You have been logged out successfully.');
+        window.location.href = 'login';
+    </script>";
+    exit;
+}
+?>
+
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow" data-bs-theme="dark">
   <div class="container">
-    <a class="navbar-brand" href="#">
-        <img src="../img/break point.jpg" alt="Logo" width="35" height="34" class="d-inline-block align-text-top">
+    <a class="navbar-brand" href="/break-point/admin">
+        <img src="/break-point/img/break point.jpg" alt="Logo" width="35" height="34" class="d-inline-block align-text-top">
         Break Point
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,27 +27,33 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          <a class="nav-link active" aria-current="page" href="/break-point/admin">Dashboard</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Billboards</a>
+          <a class="nav-link" href="/break-point/admin/billboards">Billboards</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Menu Category</a>
+          <a class="nav-link" href="/break-point/admin/category">Menu Category</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Menu Items</a>
+          <a class="nav-link" href="/break-point/admin/items">Menu Items</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Settings</a>
+          <a class="nav-link" href="/break-point/admin/settings">Settings</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Log Out</a>
+          <a class="nav-link" href="#" onclick="confirmLogout()">Log Out</a>
         </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li> -->
       </ul>
     </div>
   </div>
 </nav>
+
+<script>
+    function confirmLogout() {
+
+        if (confirm('Are you sure you want to log out?')) {
+            window.location.href = '?action=logout';
+        }
+    }
+</script>
