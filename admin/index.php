03392378bf42a4b617fdf8cbe12190edecdfee2e
Alam-Monir <monirrza0641@gmail.com>
2024-10-23 04:23:56 +0530

    added session check for admin

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    
+    header("Location: login");
+    exit;
+}
+?>
+
 <?php include('includes/header.php')?>
 <?php include('includes/navbar.php')?>
 <h1>Hello, Admin anna!</h1>
