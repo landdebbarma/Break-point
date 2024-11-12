a000d1577ab2f1a4d81d2f2724de4b0e8efba494
Alam-Monir <monirrza0641@gmail.com>
2024-11-12 14:55:25 +0530

    beverages fix

--- beverages/index.php
+++ beverages/index.php
@@ -2,13 +2,84 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, intial-scale=1.0" />
+  <title>BREAK POINT</title>
+  <link rel="stylesheet" href="/break-point/admin/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+  <link rel="stylesheet" href="../style.css" />
+  <style>
+    /* sliding offer */
+    .section-2-container {
+      margin-top: 170px;
+      margin-bottom: 30px;
+      width: 100%;
+      height: 160px;
+      object-fit: cover;
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      animation-name: slide;
+      animation-duration: 10s;
+      animation-iteration-count: infinite;
+    }
+  </style>
 </head>
 
+<!--navbar top-->
+
 <body>
-    this is beverages page
+  <nav>
+    <div class="container1">
+      <a href="../" class="logo">
+        <img src="../img/break point.png" alt="Logo" />
+      </a>
+    </div>
+    <div class="text1">
+      <h2>YOUR HUB TO<br>Relish Delights</h2>
+    </div>
+  </nav>
+
+  <!--menu items-->
+
+  <section class="container2">
+    <div class="section-1-container">
+      <div class="section-1-items">
+        <a href="starters/"><img src="../img/burger.png" /></a>
+      </div>
+      <div class="section-1-items">
+        <a href="items/"><img src="../img/701965.png" /></a>
+      </div>
+      <div class="section-1-items">
+        <a href="#"><img src="../img/17868004.png" /></a>
+      </div>
+      <div class="section-1-items">
+        <a href="default.asp"> <img src="../img/3081098.png" /></a>
+      </div>
+    </div>
+  </section>
+
+  <!--terms & condition-->
+
+  <section id="footer">
+    <div>
+      <a href="contacts/" class="footer-title">address</a>
+    </div>
+    <div>
+      <a href="contacts/" class="footer-title">contacts</a>
+    </div>
+    <div>
+      <a href="contacts/" class="footer-title">Developer</a>
+    </div>
+  </section>
+
+  <section id="copyright">
+    <p>&copy Break point | All rights reserved</p>
+  </section>
 </body>
 
 </html>
+
+<script src="/break-point/admin/assets/js/jquery-3.7.1.min.js"></script>
+<script src="/break-point/admin/assets/js/bootstrap.bundle.min.js"></script>
