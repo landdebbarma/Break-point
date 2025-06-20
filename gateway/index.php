37bf34a0f71e6f08bf1a80bb365eb472bd537e37
landdebbarma <landdebbarma5@gmail.com>
2025-06-21 01:07:39 +0530

    payment gatway

---
+++ gateway/index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>BREAK POINT</title>
+
+    <link rel="stylesheet" href="/break-point/admin/assets/css/bootstrap.min.css">
+    <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Audiowide|Sofia|Trirong" />
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+    <link rel="stylesheet" href="../style.css">
+
+
+<body>
+
+    <!--input details-->
+
+    <div class="payment-box" style="width: 100%;border:none;">
+
+        <h5 class="ms-3 fw-bold mt-5">Payment</h5>
+
+        <h5 class="ms-3 mt-5 fs-6 fw-bold">Card Details</h5>
+
+        <div class="input-group mb-3 ms-3 mt-4" style="width: 92%; height: 50px;">
+            <input type="text" class="form-control" placeholder="card number">
+        </div>
+
+        <div class="d-flex flex-row mx-3 mt-5">
+            <div class="input-group mb-3" style="width: 100%;">
+                <input type="text" class="form-control" placeholder="Expariy Date">
+            </div>
+
+            <div class="input-group mb-3 ms-3" style="width: 100%; height: 50px;">
+                <input type="text" class="form-control" placeholder="CVV">
+            </div>
+        </div>
+
+        <div class="input-group mb-3 ms-3 mt-4" style="width: 92%; height: 50px;">
+            <input type="text" class="form-control" placeholder="Card Holder Name">
+        </div>
+    </div>
+
+    <!--payment details-->
+    <div class="text-center mx-3 mt-5 mb-5">
+        <h4 class="text-secondary">All Payment Are Secured</h4>
+        <div class="d-flex flex-row justify-content-center mt-3 gap-3">
+
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
+                <path fill="#e64a19" d="M42.858,11.975c-4.546-2.624-10.359-1.065-12.985,3.481L23.25,26.927	c-1.916,3.312,0.551,4.47,3.301,6.119l6.372,3.678c2.158,1.245,4.914,0.506,6.158-1.649l6.807-11.789	C48.176,19.325,46.819,14.262,42.858,11.975z"></path>
+                <path fill="#fbc02d" d="M35.365,16.723l-6.372-3.678c-3.517-1.953-5.509-2.082-6.954,0.214l-9.398,16.275	c-2.624,4.543-1.062,10.353,3.481,12.971c3.961,2.287,9.024,0.93,11.311-3.031l9.578-16.59	C38.261,20.727,37.523,17.968,35.365,16.723z"></path>
+                <path fill="#43a047" d="M36.591,8.356l-4.476-2.585c-4.95-2.857-11.28-1.163-14.137,3.787L9.457,24.317	c-1.259,2.177-0.511,4.964,1.666,6.22l5.012,2.894c2.475,1.43,5.639,0.582,7.069-1.894l9.735-16.86	c2.017-3.492,6.481-4.689,9.974-2.672L36.591,8.356z"></path>
+                <path fill="#1e88e5" d="M19.189,13.781l-4.838-2.787c-2.158-1.242-4.914-0.506-6.158,1.646l-5.804,10.03	c-2.857,4.936-1.163,11.252,3.787,14.101l3.683,2.121l4.467,2.573l1.939,1.115c-3.442-2.304-4.535-6.92-2.43-10.555l1.503-2.596	l5.504-9.51C22.083,17.774,21.344,15.023,19.189,13.781z"></path>
+            </svg>
+            <img width="30" height="30" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-visa-an-american-multinational-financial-services-corporation-logo-shadow-tal-revivo.png" alt="external-visa-an-american-multinational-financial-services-corporation-logo-shadow-tal-revivo" />
+
+            <img width="48" height="48" src="https://img.icons8.com/color/48/rupay.png" alt="rupay" style="margin-top: -10px;" />
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 48 48">
+                <path fill="#0d47a1" d="M5.446 18.01H.548c-.277 0-.502.167-.503.502L0 30.519c-.001.3.196.45.465.45.735 0 1.335 0 2.07 0C2.79 30.969 3 30.844 3 30.594 3 29.483 3 28.111 3 27l2.126.009c1.399-.092 2.335-.742 2.725-2.052.117-.393.14-.733.14-1.137l.11-2.862C7.999 18.946 6.949 18.181 5.446 18.01zM4.995 23.465C4.995 23.759 4.754 24 4.461 24H3v-3h1.461c.293 0 .534.24.534.535V23.465zM13.938 18h-3.423c-.26 0-.483.08-.483.351 0 .706 0 1.495 0 2.201C10.06 20.846 10.263 21 10.552 21h2.855c.594 0 .532.972 0 1H11.84C10.101 22 9 23.562 9 25.137c0 .42.005 1.406 0 1.863-.008.651-.014 1.311.112 1.899C9.336 29.939 10.235 31 11.597 31h4.228c.541 0 1.173-.474 1.173-1.101v-8.274C17.026 19.443 15.942 18.117 13.938 18zM14 27.55c0 .248-.202.45-.448.45h-1.105C12.201 28 12 27.798 12 27.55v-2.101C12 25.202 12.201 25 12.447 25h1.105C13.798 25 14 25.202 14 25.449V27.55zM18 18.594v5.608c.124 1.6 1.608 2.798 3.171 2.798h1.414c.597 0 .561.969 0 .969H19.49c-.339 0-.462.177-.462.476v2.152c0 .226.183.396.422.396h2.959c2.416 0 3.592-1.159 3.591-3.757v-8.84c0-.276-.175-.383-.342-.383h-2.302c-.224 0-.355.243-.355.422v5.218c0 .199-.111.316-.29.316H21.41c-.264 0-.409-.143-.409-.396v-5.058C21 18.218 20.88 18 20.552 18c-.778 0-1.442 0-2.22 0C18.067 18 18 18.263 18 18.594L18 18.594z"></path>
+                <path fill="#00adee" d="M27.038 20.569v-2.138c0-.237.194-.431.43-.431H28c1.368-.285 1.851-.62 2.688-1.522.514-.557.966-.704 1.298-.113L32 18h1.569C33.807 18 34 18.194 34 18.431v2.138C34 20.805 33.806 21 33.569 21H32v9.569C32 30.807 31.806 31 31.57 31h-2.14C29.193 31 29 30.807 29 30.569V21h-1.531C27.234 21 27.038 20.806 27.038 20.569L27.038 20.569zM42.991 30.465c0 .294-.244.535-.539.535h-1.91c-.297 0-.54-.241-.54-.535v-6.623-1.871c0-1.284-2.002-1.284-2.002 0v8.494C38 30.759 37.758 31 37.461 31H35.54C35.243 31 35 30.759 35 30.465V18.537C35 18.241 35.243 18 35.54 18h1.976c.297 0 .539.241.539.537v.292c1.32-1.266 3.302-.973 4.416.228 2.097-2.405 5.69-.262 5.523 2.375 0 2.916-.026 6.093-.026 9.033 0 .294-.244.535-.538.535h-1.891C45.242 31 45 30.759 45 30.465c0-2.786 0-5.701 0-8.44 0-1.307-2-1.37-2 0v8.44H42.991z"></path>
+            </svg>
+        </div>
+
+    </div>
+
+
+    <!--pay button-->
+    <div class="d-flex flex-row justify-content-center mt-5">
+        <a class="bg-danger bg-gradient rounded-2 fw-bold d-flex align-items-center justify-content-center text-white text-decoration-none"
+            href="../index.html"
+            style="width: 100%; height: 50px; margin-top: 30px;">
+            Pay Now
+        </a>
+    </div>
+
+
+
+</body>
+
+</html>
+
+<script src="/break-point/admin/assets/js/jquery-3.7.1.min.js"></script>
+<script src="/break-point/admin/assets/js/bootstrap.bundle.min.js"></script>
