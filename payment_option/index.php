92cf74982296b52a4ad85364a4ae213682b1928f
landdebbarma <landdebbarma5@gmail.com>
2025-07-05 12:47:49 +0530

    option

---
+++ payment_option/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Payment</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="card d-flex flex-row mt-3 p-2 mx-2 justify-content-around align-items-center">
+        <img width="30" height="30" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/external-visa-an-american-multinational-financial-services-corporation-logo-shadow-tal-revivo.png" alt="external-visa-an-american-multinational-financial-services-corporation-logo-shadow-tal-revivo" />
+        <p class="m-1 fw-bold">***************8797</p>
+        <div class="card d-flex flex-row align-items-center p-2 bg-black text-white" style="border-radius: 10px; width: auto; height: auto;">
+
+            <div class="d-flex flex-row align-items-center">
+                <a href="" class="fs-6 m-0 cursor-pointer text-decoration-none text-light">Pay Now</a>
+            </div>
+
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
